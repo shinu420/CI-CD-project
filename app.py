5c8c7b336b8aac1653d28eebdbeff3492cf1ad79
Amit kumar <amit249195@gmail.com>
2026-02-01 14:07:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask 
-app  = Flask(__name__)
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return "hello docker!"
-if __name__== "__main__":
-   app.run(host='0.0.0.0' , port=5000)
+    return "Hello from CI/CD Pipeline 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
